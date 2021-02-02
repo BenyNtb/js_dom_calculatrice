95aa110936d01ef9a3a38e171a3636e3ceec48aa
Beny <b.ntambwe97@gmail.com>
2021-02-02 22:45:08 +0100

    javascript complet, design a modifier

---
+++ public/js/calculatrice.js
@@ -0,0 +1,17 @@
+// import {touches,listeKeycode,ecran} from './Fonction.js';
+// import {calculer} from './Fonction.js'
+// import {} from './Fonction.js'
+// import {} from './Fonction.js'
+// import {} from './Fonction.js'
+// import {} from './Fonction.js'
+// import {} from './Fonction.js'
+// import {} from './Fonction.js'
+
+// console.log(touches,listeKeycode,ecran,valeur,calcul,indexKeycode,touche);
+// console.log(calculer("calcul,indexKeyCode,touche"));
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
